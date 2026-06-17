6ab0c4f5c8c4044236e95b177495ba1acc4803e4
Harish Ubuntu <harish.faqot02@gmail.com>
2026-06-17 20:00:13 +0700

    add controller

---
+++ build/robotpadi_description/prefix_override/sitecustomize.py
@@ -0,0 +1,4 @@
+import sys
+if sys.prefix == '/usr':
+    sys.real_prefix = sys.prefix
+    sys.prefix = sys.exec_prefix = '/home/ubuntu/github/Omni-Directional-Mobile-Robot/install/robotpadi_description'
---
+++ build/robotpadi_description/share/robotpadi_description/hook/pythonpath_develop.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/hook_prepend_value.sh.em
+
+_colcon_prepend_unique_value PYTHONPATH "/home/ubuntu/github/Omni-Directional-Mobile-Robot/build/robotpadi_description"
